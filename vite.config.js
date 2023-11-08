8e439476d65c886753e2318d37881064db9ca389
Mariia Postovik <accaunt01.13@gmail.com~>
2023-11-08 22:07:49 +0200

    2 try

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import inject from '@rollup/plugin-inject';
+import htmlPurge from 'vite-plugin-purgecss';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    inject({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+    htmlPurge(),
+  ],
+  css: {
+    devSourcemap: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        productsListing: resolve(__dirname, 'product-listing.html')
+      }
+    }
+  }
+})
